f769fbacbd0038cf3b87d37869ac849a21b74880
Patsanoob <63359116+Patsanoob@users.noreply.github.com>
2020-09-23 15:59:50 +0200

    Patrick Hover et Click
    Fonctionnel reste juste a les rajouter sur le produit final

---
+++ Menu Project Unity/Assets/Scenes/Patrick Assets/Mouse_Hovering_Clicking.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse_Hovering_Clicking : MonoBehaviour
+{
+    public Outline Outline_Script;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Outline_Script.OutlineWidth = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseEnter()
+    {
+        Outline_Script.OutlineWidth = 7.0f;
+    }
+    private void OnMouseExit()
+    {
+        Outline_Script.OutlineWidth = 0.0f;
+    }
+}
